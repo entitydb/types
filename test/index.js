a3ce824c75e2385faadea6db8b0fadbae01d49ee
Michael Williams <dinosaur@riseup.net>
2014-05-14 03:49:41 -0700

    use tv4 instead of home brewed schema system

--- test/index.js
+++ test/index.js
@@ -8,4 +8,37 @@ describe("Type", function () {
     Type = require('../');
     expect(Type).to.exist;
   });
+
+  it("should extend Type to Thing", function () {
+    var Thing = Type.extend({
+      type: "Thing",
+      object: {
+        properties: {
+          name: {
+            type: "string",
+          },
+        },
+      },
+    });
+  });
+
+  it("should error extending Type", function () {
+    var fn = function () {
+      var Thing = Type.extend({});
+    };
+    expect(fn).to.throw(Type.TypeError);
+  });
+
+  it("should extend Type to Thing", function () {
+    var Thing = Type.extend({
+      type: "Thing",
+      object: {
+        properties: {
+          name: {
+            type: "string",
+          },
+        },
+      },
+    });
+  });
 });
